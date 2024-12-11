c8176880308a5fedda3685081fb58296054be93e
DudaAndrei03 <160632807+DudaAndrei03@users.noreply.github.com>
2024-12-12 00:53:26 +0200

    Update client.py

--- client.py
+++ client.py
@@ -70,7 +70,7 @@ class CoAPClient:
         message_type = 1
         token_length = 4
         #-
-        code = 3  # GET request in CoAP
+        code = 1
         message_id = 16
         token = b'\xA1\xB2\xC3\xD4'
         payload_marker=b'\xFF'
@@ -89,9 +89,9 @@ class CoAPClient:
         file_content='Test create file'
         file_content_bytes=file_content.encode('utf-8')
         data = json.dumps({'CATEGORY' : 'FILE',
-                           'OP' : 'UPLOAD',
-                            'PARAM1':'file_test.txt',
-                           'PARAM2':file_content
+                           'OP' : 'DOWNLOAD',
+                            'PARAM1':'DIR_TEST_MAKEDIR/DIRECTOR2/DIRECTOR3/TESTDOWNLOAD.txt',
+                           'PARAM2':'DIR_TEST_MAKEDIR/DIRECTOR2'
                            }).encode('utf-8')
         #data->payload
         #converteste string-ul cu format standardizat de tip JSON catre bytes pentru a putea fi transmis la server
@@ -104,7 +104,7 @@ class CoAPClient:
             parsed_response=self.parse_header(response)
 
             print(parsed_response.code)
-            print(f"Raspuns primit de la server: {parsed_response.payload.decode('utf-8')}")
+            print(f"Raspuns primit de la server: {parsed_response.payload.decode('utf-8')},")
 
         except socket.timeout:
             print("Nu s-a primit niciun raspuns de la server.")
